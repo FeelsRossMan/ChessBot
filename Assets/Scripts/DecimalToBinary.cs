0079645e2c3a551c67716ae71a7c9198e0c24343
Evan <25727163+FeelsRossMan@users.noreply.github.com>
2023-08-10 14:01:59 -0700

    utility function for converting int to binary

---
+++ Assets/Scripts/DecimalToBinary.cs
@@ -0,0 +1,10 @@
+public static class DecimalToBinary
+{
+    static void Main() 
+    {
+        string returnString = "10";
+        int current = ulong.Parse(c);
+        string binString = System.Convert.ToString(current, 2);
+        System.WriteLine(binString);
+    }
+}
